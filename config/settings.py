025ebc7d91f3cc9bf85795639fff62558cdd23e5
miyou995 <inter.taki@gmail.com>
2022-01-13 12:20:36 +0100

    hey assets

--- config/settings.py
+++ config/settings.py
@@ -145,9 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    ]
+
 
 STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL ='/media/'
